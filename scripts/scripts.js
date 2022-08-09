e7c55819f8cf0a103c5817687318a9b18f80e0f7
Lucas Kaique <lucaskaique743@gmail.com>
2022-08-09 20:46:57 -0300

    Melhorias

---
+++ scripts/scripts.js
@@ -0,0 +1,62 @@
+function detailsHtml() {
+    let title = document.getElementById("tech-skill");
+    let description = document.getElementById("skill-description");
+
+    title.innerHTML = "HTML"  
+    description.innerHTML = "Considero que tenho um bom conhecimento."
+}
+
+function detailsCss() {
+    let title = document.getElementById("tech-skill");
+    let description = document.getElementById("skill-description");
+
+    title.innerHTML = "CSS"  
+    description.innerHTML = "Considero que tenho um conhecimento médio."
+}
+
+function detailsJs() {
+    let title = document.getElementById("tech-skill");
+    let description = document.getElementById("skill-description");
+
+    title.innerHTML = "JavaScript"  
+    description.innerHTML = "Considero que tenho um conhecimento médio."
+}
+
+function detailsReact() {
+    let title = document.getElementById("tech-skill");
+    let description = document.getElementById("skill-description");
+
+    title.innerHTML = "React Js"  
+    description.innerHTML = "Comecei os estudos recentemente, ainda tenho muito a evoluir."
+}
+
+function detailsNode() {
+    let title = document.getElementById("tech-skill");
+    let description = document.getElementById("skill-description");
+
+    title.innerHTML = "Node Js"  
+    description.innerHTML = "Comecei os estudos recentemente, ainda tenho muito a evoluir."
+}
+
+function detailsGit() {
+    let title = document.getElementById("tech-skill");
+    let description = document.getElementById("skill-description");
+
+    title.innerHTML = "Git"  
+    description.innerHTML = "Considero que tenho um conhecimento médio"
+}
+
+function detailsGitHub() {
+    let title = document.getElementById("tech-skill");
+    let description = document.getElementById("skill-description");
+
+    title.innerHTML = "GitHub"  
+    description.innerHTML = "Considero que tenho um conhecimento médio."
+}
+
+
+
+
+
+
+
